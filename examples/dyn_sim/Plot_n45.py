4b7e39009970cf943bd5bea72081f9150e3a5daa
nslillel <nslillel@stud.ntnu.no>
2025-02-03 14:46:06 +0100

    bug fix

--- examples/dyn_sim/Plot_n45.py
+++ examples/dyn_sim/Plot_n45.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('/Users/joerg/Documents/NTNU/Master/TOPS_low_inertia/')
-import inertia_sim.utility_functions as uf
+import inertia_sim.utility_functions_NJ as uf
 
 if __name__ == '__main__':  
     folder_path = '/Users/joerg/Documents/NTNU/Master/TOPS_low_inertia/Results/Base/'
