4a01d72a2878fbe3704aa0db43b6d89f7f505dfc
jorgekso <146705858+jorgekso@users.noreply.github.com>
2025-02-03 13:35:09 +0100

    Added gen_trip.py and plot_n45

---
+++ examples/dyn_sim/Plot_n45.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/Users/joerg/Documents/NTNU/Master/TOPS_low_inertia/')
+import inertia_sim.utility_functions as uf
+
+if __name__ == '__main__':  
+    folder_path = '/Users/joerg/Documents/NTNU/Master/TOPS_low_inertia/Results/Base/'
+    results,file_names = uf.format_results(folder_path)
+    uf.plot_freq(results, file_names)
