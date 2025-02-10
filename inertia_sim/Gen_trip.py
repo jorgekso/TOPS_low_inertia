6b5b2a4899de60d9b87a061ed8bed3e896250a41
nslillel <nslillel@stud.ntnu.no>
2025-02-10 13:30:39 +0100

    reorderd file structure, inertia_sim/not_in_use -> files we don´t use

--- inertia_sim/Gen_trip.py
+++ inertia_sim/Gen_trip.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     #This code is used to generate a trip of a 1400MW generator in the 3359 bus of the Nordic 45 system.
-    import n45_functions as func
+    import inertia_sim.not_in_use.n45_functions as func
     #iterates over the different kinetic energy scenarios
     
     func.gen_trip('7000',1400,1400,300*1e3,'pf_tests/negative_flow_with_fault',0,70,37.6,True, True)
