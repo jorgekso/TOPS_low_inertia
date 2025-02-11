4e025ceaa2bbb756cf064e93ffc042bc0191fb6f
nslillel <nslillel@stud.ntnu.no>
2025-02-11 10:01:07 +0100

    added a display_pf and init_n45_with_vsc functions

--- inertia_sim/not_in_use/power_flow.py
+++ inertia_sim/not_in_use/power_flow.py
@@ -1,24 +1,14 @@
 
 import tops.ps_models.n45_with_controls_HVDC as model_data 
-from inertia_sim.not_in_use.n45_functions import init_n45 
+from init_N45 import init_n45, display_power_flow, init_n45_with_VSC
+
 import pandas as pd
-
-
-        
-    
-    
-
-
-
-
-
-
 
 if __name__ == '__main__':
 
-    ps = init_n45()
+    ps = init_n45_with_VSC(model_data, True)
 
-    print(ps.gen['GEN'].par['name'])
+    
 
     
 
