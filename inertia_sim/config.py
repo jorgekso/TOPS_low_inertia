49387855e9fda3b5ff65b0059a7fe4060ce3323d
nslillel <nslillel@stud.ntnu.no>
2025-02-18 14:10:59 +0100

    updated gitignore

--- inertia_sim/config.py
+++ inertia_sim/config.py
@@ -2,4 +2,4 @@
 
 
 # should be changed to the users own path:
-system_path = '/Users/noralillelien/Documents/TOPS_low_inertia/''' # Path to the system data
+system_path = '/Users/noralillelien/Documents/TOPS_low_inertia/' # Path to the system data
