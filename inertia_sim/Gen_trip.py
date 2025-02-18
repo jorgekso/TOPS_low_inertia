34020cdedb5db0dab2f75e593bce74e52023dee9
nslillel <nslillel@stud.ntnu.no>
2025-02-18 11:03:15 +0100

    changed generation data

--- inertia_sim/Gen_trip.py
+++ inertia_sim/Gen_trip.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     import tops.ps_models.n45_with_controls_HVDC as n45
     ps = func.init_n45_with_VSC(model_data=n45,display_pf=True)
     # func.run_sensitivity(ps,'r',[3.5,3,2.5,2,1.5],foldername = 'r_sensitivity/')
-    func.gen_trip(ps=ps,fault_bus = '5230',fault_Sn = 792,fault_P = 792,kinetic_energy_eps = 300e3, folderandfilename = 'NordLink/test1',t=0,t_end=50,t_trip = 17.6,event_flag = True,VSC=False)
-    
+    #func.gen_trip(ps=ps,fault_bus = '5230',fault_Sn = 792,fault_P = 792,kinetic_energy_eps = 300e3, folderandfilename = 'NordLink/test1',t=0,t_end=50,t_trip = 17.6,event_flag = True,VSC=False)
+    func.gen_trip(ps=ps,folderandfilename = 'NordLink/base_nofault', event_flag=False)
