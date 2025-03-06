cd58d93957961f7389095067ff1e11e089cce844
nslillel <nslillel@stud.ntnu.no>
2025-03-06 08:13:22 +0100

    added activated FFR and moved event to sepearte .py file

---
+++ inertia_sim/FFR.py
@@ -0,0 +1,87 @@
+import numpy as np 
+
+
+def activate_FFR(ps, mean_freq,t, vsc_names,activated):
+    
+    threshold = 49.7
+   
+    if (mean_freq <= threshold) and activated == False:
+        activated = True
+        print(f'FFR activated at t = {t}')
+        P_FFR = 50 #Max FFR 50 MW
+
+        for name in ps.vsc['VSC_SI'].par['name']:
+
+            if name in vsc_names:
+                idx = np.where(ps.vsc['VSC_SI'].par['name'] == name)[0][0]
+                p_pre = ps.vsc['VSC_SI'].par['p_ref'][idx]
+                p_new = p_pre + P_FFR/ps.vsc['VSC_SI'].par['S_n'][idx]
+
+                if p_new > ps.vsc['VSC_SI'].par['S_n'][idx]:
+                    p_new = ps.vsc['VSC_SI'].par['S_n'][idx]
+                ps.vsc['VSC_SI'].set_input('p_ref', p_new, idx)
+
+                print(f'FFR activated at {name}')
+                print(f'power injected = {P_FFR + p_pre*ps.vsc['VSC_SI'].par['S_n'][idx]} MW')
+    return activated       
+
+            
+
+if __name__ == '__main__':
+    
+    import init_N45 as func
+    import tops.ps_models.n45_with_controls_HVDC as n45
+
+    from inertia_sim.fault_events import gen_trip, HVDC_cable_trip
+
+
+    energy_mix = {'FI': {'Wind': 0.7, 'Hydro': 0.1, 'Nuclear': 0.2, 'Solar': 0.0, 'Fossil': 0.0},
+                'NO_1': {'Wind': 0.6, 'Hydro': 0.4, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'NO_2': {'Wind': 0.6, 'Hydro': 0.4, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'NO_3': {'Wind': 0.4, 'Hydro': 0.6, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'NO_4': {'Wind': 0.5, 'Hydro': 0.5, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'NO_5': {'Wind': 0.0, 'Hydro': 1.0, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'SE_1': {'Wind': 0.95, 'Hydro': 0.05, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'SE_2': {'Wind': 0.95, 'Hydro': 0.05, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0},
+                'SE_3': {'Wind': 0.6, 'Hydro': 0.0, 'Nuclear': 0.4, 'Solar': 0.0, 'Fossil': 0.0},
+                'SE_4': {'Wind': 0.95, 'Hydro': 0.05, 'Nuclear': 0.0, 'Solar': 0.0, 'Fossil': 0.0}}
+
+
+    ps = func.init_n45(model_data=n45,display_pf=False,energy_mix= energy_mix, 
+                       data_path='inertia_sim/2030_scenario/',
+                       kinetic_energy_eps=150e3,virtual_gen=False,spinning_reserve=4)
+    
+    
+    SE_4 = ['WG8500-1', 'WG8500-2']
+    SE3 = ['WG3000-1', 'WG3000-2', 'WG3300-1', 'WG3300-2', 'WG3359-1', 'WG3359-2']
+    SE2 = ['WG3100-1', 'WG3100-2', 'WG3100-3', 'WG3245-1', 'WG3245-2', 'WG3245-3']
+    SE1 = ['WG3115-1', 'WG3249-1', 'WG3249-2'] 
+    NO_1 =  ['WG5120-1']
+    NO_2 = ['WG5230-1', 'WG5230-2']
+    NO_3 = ['WG5320-1']
+    NO_4 = ['WG5420-1', 'WG5420-1']
+    FI = ['WG7000-1', 'WG7000-2', 'WG7000-3', 'WG7100-1', 'WG71000-2', 'WG71000-3']
+
+    FFR_sources= ['WG7000-1']
+
+    
+    HVDC_cable_trip(ps,folderandfilename = 'FFR_FI/50 MW',t=0,t_end=50,t_trip = 17.6,event_flag = True, 
+                    link_name = 'NO_2-DE',FFR = True, FFR_sources = FFR_sources)
+    # func.run_sensitivity(ps,'r',[3.5,3,2.5,2,1.5],foldername = 'r_sensitivity/')
+    #func.gen_trip(ps=ps,fault_bus = '5230',fault_Sn = 792,fault_P = 792,kinetic_energy_eps = 300e3, 
+    # folderandfilename = 'NordLink/test1',t=0,t_end=50,t_trip = 17.6,event_flag = True,VSC=False)
+    # func.gen_trip(ps=ps,folderandfilename = '2030_scenario/test', fault_bus = '5230',fault_Sn = 1400,
+    #               fault_P = 1400,event_flag=True, VSC=True, t_trip = 10.81)
+
+    # func.HVDC_cable_trip(ps=ps,folderandfilename = 'Spinning reserves/2030 0,76',
+    #                      link_name = 'NO_2-DE',t_trip=10.81,event_flag=False,t_end=50)
+    
+
+    
+                
+
+
+
+
+
+
---
+++ inertia_sim/fault_events.py
@@ -0,0 +1,196 @@
+import sys
+from collections import defaultdict
+import time
+
+from config import system_path
+sys.path.append(system_path)  # Corrected path to dyn_sim module
+
+import tops.dynamic as dps
+import tops.solvers as dps_sol
+import importlib
+importlib.reload(dps)
+
+
+import utility_functions_NJ as uf
+import numpy as np 
+
+
+from FFR import activate_FFR
+
+
+def gen_trip(ps,folderandfilename, fault_bus = '7000',fault_Sn = 1400,fault_P = 1400,kinetic_energy_eps = 300e3,
+             t=0,t_end=50,t_trip = 17.6,event_flag = True,VSC=False, FFR = False, FFR_vsc_names = None):
+    """
+    Simulates a generator trip in the Nordic 45 system.
+    Parameters:
+    model_data : dictionary
+        Dictionary containing the model data.
+    fault_bus : string
+        The bus number of the fault.
+    fault_Sn : float
+        The nominal power of the fault.
+    fault_P : float
+        The active power of the fault.
+    kinetic_energy_eps : float
+        The kinetic energy of the EPS.
+    folderandfilename : string
+        The folder and filename of the results.
+    t : float
+        The initial time of the simulation.
+    t_end : float
+        The end time of the simulation.
+    t_trip : float
+        The time of the generator trip.
+    event_flag : bool
+        If True, the generator trip event is triggered.
+    VSC : bool
+        If True, the VSC results are included in the simulation.
+    """
+    
+    ps.power_flow()
+    ps.init_dyn_sim()
+    x0 = ps.x0.copy()
+    v0 = ps.v0.copy()
+
+
+
+
+    x_0 = ps.x_0.copy()
+
+    # Solver
+    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0, t_end, max_step=5e-3)
+
+    # Initialize simulation
+    t = 0
+    res = defaultdict(list)
+    t_0 = time.time()
+
+        
+    FFR_activated = False
+    while t < t_end:
+        sys.stdout.write("\r%d%%" % (t/(t_end)*100))
+
+        if t > t_trip and event_flag:
+            event_flag = False
+            ps.lines['Line'].event(ps, 'Virtual line', 'disconnect')
+        result = sol.step()
+        x = sol.y
+        v = sol.v
+        t = sol.t
+
+
+
+        
+
+
+
+        dx = ps.ode_fun(0, ps.x_0)
+
+
+        res['t'].append(t)
+        res['gen_speed'].append(ps.gen['GEN'].speed(x, v).copy())
+        res['v'].append(v.copy())
+        res['gen_I'].append(ps.gen['GEN'].I(x, v).copy())
+        res['gen_P'].append(ps.gen['GEN'].P_e(x, v).copy())
+        res['load_P'].append(ps.loads['Load'].P(x, v).copy())
+        res['load_Q'].append(ps.loads['Load'].Q(x, v).copy())
+        if(VSC):
+            res['VSC_p'].append(ps.vsc['VSC_SI'].p_e(x, v).copy())
+            res['VSC_Sn'].append(ps.vsc['VSC_SI'].par['S_n'])
+            res['VSC_name'].append(ps.vsc['VSC_SI'].par['name'])
+    
+    disconnected_gen_idx = -1  # Index of the disconnected generator, should be the last one bc of the virtual generator added.
+    # Deletes the disconnected generator from the results
+    # Convert lists to numpy arrays before deleting elements
+    for key in ['gen_speed', 'gen_I', 'gen_P']:
+        res[key] = np.array(res[key])
+        res[key] = np.delete(res[key], disconnected_gen_idx, axis=1)
+        res[key] = res[key].tolist()  # Convert back to list for JSON serialization
+    res['gen_name'] = np.array(ps.gen['GEN'].par['name'])
+    res['gen_name'] = np.delete(res['gen_name'], disconnected_gen_idx)
+    res['gen_name'] = res['gen_name'].tolist()  # Convert back to list for JSON serialization
+
+    res['bus_names'].append(ps.buses['name'])
+    print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
+    uf.read_to_file(res, 'Results/'+folderandfilename+'.json')
+
+
+def HVDC_cable_trip(ps,folderandfilename,t=0,t_end=50,t_trip = 17.6,event_flag = True, link_name = 'NO_2-DE', FFR = False, FFR_sources = None):
+    ''''
+    Simulates a trip of a HVDC cable in the Nordic 45 system.
+    Parameters:
+    ps : PowerSystemModel
+        The power system model.
+    folderandfilename : string
+        The folder and filename of the results.
+    t : float
+        The initial time of the simulation.
+    t_end : float
+        The end time of the simulation.
+    t_trip : float
+        The time of the trip.
+    event_flag : bool
+        If True, the trip event is triggered.
+    line : string
+        The name of the line that should trip.
+    '''
+    
+    ps.power_flow()
+    ps.init_dyn_sim()
+    x0 = ps.x0.copy()
+    v0 = ps.v0.copy()
+
+
+
+
+    x_0 = ps.x_0.copy()
+
+    # Solver
+    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0, t_end, max_step=5e-3)
+
+    # Initialize simulation
+    t = 0
+    res = defaultdict(list)
+    t_0 = time.time()
+
+
+    FFR_activated = False
+        
+    
+    while t < t_end:
+        sys.stdout.write("\r%d%%" % (t/(t_end)*100))
+
+        if t > t_trip and event_flag:
+            event_flag = False
+            #find the index of the vsc corresponding to the name load 
+            #ps.vsc['VSC_SI'].par['name'] is an ndarray
+            index_vsc = np.where(ps.vsc['VSC_SI'].par['name'] == link_name)[0]
+            ps.vsc['VSC_SI'].set_input('p_ref', 0,index_vsc)
+        result = sol.step()
+        x = sol.y
+        v = sol.v
+        t = sol.t
+        
+
+
+        if FFR == True:
+            mean_freq = 50 + 50*np.mean(ps.gen['GEN'].speed(x,v))
+            FFR_activated = activate_FFR(ps, mean_freq,t, FFR_sources, FFR_activated)
+
+
+        dx = ps.ode_fun(0, ps.x_0)
+        res['t'].append(t)
+        res['gen_speed'].append(ps.gen['GEN'].speed(x, v).copy())
+        res['v'].append(v.copy())
+        res['gen_I'].append(ps.gen['GEN'].I(x, v).copy())
+        res['gen_P'].append(ps.gen['GEN'].P_e(x, v).copy())
+        res['load_P'].append(ps.loads['Load'].P(x, v).copy())
+        res['load_Q'].append(ps.loads['Load'].Q(x, v).copy())
+        res['VSC_p'].append(ps.vsc['VSC_SI'].p_e(x, v).copy())
+        res['VSC_Sn'].append(ps.vsc['VSC_SI'].par['S_n'])
+        res['VSC_name'].append(ps.vsc['VSC_SI'].par['name'])
+
+    res['gen_name'].append(ps.gen['GEN'].par['name'])
+    res['bus_names'].append(ps.buses['name'])
+    print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
+    uf.read_to_file(res, 'Results/'+folderandfilename+'.json')
